dae82779dc59b1d4ea1cfebd3e73c11c9a4a2836
Jenci <jenci2114@gmail.com>
2021-10-10 18:05:27 -0400

    Created TodoList CRC card and class

---
+++ src/main/java/TodoList.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jenci
+ */
+public class TodoList {
+    List<Task> uncompletedList = new ArrayList<>();
+    List<Task> completedList = new ArrayList<>();
+
+
+}
