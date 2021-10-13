f972b1232eacd1e9df39653fa4548ea812b5dd51
halfxblood <tarinfinite@gmail.com>
2021-10-13 17:45:20 -0400

    Added addTask method and changed method name from completedTask to notCompleted

--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -3,20 +3,40 @@ package main.java;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class TodoList{
     private List<Task> completedlist = new ArrayList<>();
     private List<Task> incompletedlist = new ArrayList<>();
+
 
     /**
      * Check if task is completed, if it is completed then remove it from
      * incompletedlist and add to completedlist
      * @param task the task that will be removed from incompletedlist and added to completedlist if task is completed
      */
-    private void completedTask(Task task){
+    private void notCompleted(Task task){
         if(task.getCompleted()){
             incompletedlist.remove(task);
             completedlist.add(task);
         }
     }
+
+    /**
+     * Check for all tasks in uncompletedList,
+     * move the completed tasks into completedList
+     */
+    public void checkCompletionAll() {
+        for (Task task : incompletedlist) {
+            notCompleted(task);
+        }
+    }
+
+    public void addTask(Task task){
+        if(task.getCompleted()){
+            completedlist.add(task);
+        }
+        else{
+            incompletedlist.add(task);
+        }
+    }
+
 }
