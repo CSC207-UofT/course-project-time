a4df8a6a9bbc06b4ea8e2b79223ef20e3fec724e
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-10-24 17:52:19 -0400

    added managers returning readers

--- src/main/java/entity_accessors/TaskManager.java
+++ src/main/java/entity_accessors/TaskManager.java
@@ -1,7 +1,11 @@
 package main.java.entity_accessors;
 
+import java.util.Map;
+
 public interface TaskManager {
 
     boolean addTask(NewTaskData task);
+
+    Map<Integer, TaskReader> getTaskReaders();
 
 }
