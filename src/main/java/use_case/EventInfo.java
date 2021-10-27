ee0da772c15a2e66fe2fe21c09f4f13dd7a8e234
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-10-27 19:14:01 -0400

    added event presenter

--- src/main/java/use_case/EventInfo.java
+++ src/main/java/use_case/EventInfo.java
@@ -1,4 +1,16 @@
 package main.java.use_case;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public interface EventInfo {
+    String getName();
+
+    LocalDateTime getStartTime();
+
+    LocalDateTime getEndTime();
+
+    Set<String> getTags();
+
+    LocalDateTime getDates();
 }
