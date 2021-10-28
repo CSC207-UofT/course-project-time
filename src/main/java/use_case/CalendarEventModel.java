bc352d5c26420866f38fb690328e20f3c5936866
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-10-27 22:44:07 -0400

    Created the EventEntityManager Class and the EventtoEventReader class and did some adjustments to CalendarEventData class, CalendarEventModel interface and the Event Class. (#40)
    Authored-by: Tahseen <tarinfinite@gmail.com>

--- src/main/java/use_case/CalendarEventModel.java
+++ src/main/java/use_case/CalendarEventModel.java
@@ -1,4 +1,14 @@
 package main.java.use_case;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public interface CalendarEventModel {
+    public String getName();
+    public LocalDateTime getStartTime();
+    public LocalDateTime getEndTime();
+    public Set<String> getTags();
+    public LocalDate getDate();
+
 }
