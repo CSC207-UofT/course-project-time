cb4219944db8f74c756cbaefe3da3b4bd96f3695
valyippee <valerieyipfh@gmail.com>
2021-11-06 17:04:22 -0400

    restructured code to reflect new uml design

---
+++ src/main/java/interface_adapters/notification/NotificationPresenter.java
@@ -0,0 +1,4 @@
+package main.java.interface_adapters.notification;
+
+public class NotificationPresenter {
+}
--- src/main/java/use_case/notification/EmailDecorator.java
+++
@@ -1,4 +0,0 @@
-package main.java.use_case.notification;
-
-public class EmailDecorator extends NotificationBaseDecorator {
-}
