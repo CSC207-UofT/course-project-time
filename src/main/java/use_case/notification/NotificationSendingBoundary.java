5275d23d5187e463606d9257e8af0ca89cee50fd
valyippee <valerieyipfh@gmail.com>
2021-11-06 22:05:10 -0400

    added boundary model for presenter (notification)

--- src/main/java/use_case/notification/NotificationSendingBoundary.java
+++ src/main/java/use_case/notification/NotificationSendingBoundary.java
@@ -1,4 +1,19 @@
 package main.java.use_case.notification;
 
-public class NotificationDisplayBoundary {
+public class NotificationSendingBoundary {
+    private boolean emailEnabled;
+    private boolean systemEnabled;
+    private String message;
+
+    public boolean isEmailEnabled() {
+        return emailEnabled;
+    }
+
+    public boolean isSystemEnabled() {
+        return systemEnabled;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
