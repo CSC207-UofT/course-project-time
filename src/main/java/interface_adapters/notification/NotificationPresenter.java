153a2d0618392342e506abddcf5186302a8b4a08
Junru Lin <junru.lin@mail.utoronto.ca>
2021-11-07 21:19:49 -0500

    added documentations

--- src/main/java/interface_adapters/notification/NotificationPresenter.java
+++ src/main/java/interface_adapters/notification/NotificationPresenter.java
@@ -2,6 +2,13 @@ package main.java.interface_adapters.notification;
 
 import main.java.use_case.notification.NotificationSender;
 
+/**
+ * NotificationPresents is used by NotificationObserver
+ * to send notification message to user
+ *
+ * - notifier: used to send message, which is
+ *             constructed based on the notification settings
+ */
 public class NotificationPresenter implements NotificationSender {
 
     private Notifier notifier;
@@ -10,6 +17,10 @@ public class NotificationPresenter implements NotificationSender {
         resetNotification(settings);
     }
 
+    /**
+     * reset the notification settings by rebuilding the notifier
+     * @param settings new notification settings
+     */
     public void resetNotification(NotificationSettings settings) {
         this.notifier = new BasicNotifier();
         if (settings.isEmailEnabled()) {
--- src/main/java/use_case/notification/NotificationTracker.java
+++ src/main/java/use_case/notification/NotificationTracker.java
@@ -6,6 +6,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Runnable NotificationTracker that track the upcoming notifications
+ *
+ * - observerList: the observers that observe and handle new notifications
+ * - notificationList: a list of notification that will be sent to user,
+ *                     sorted based on the notification time, from earlier to later
+ */
 public class NotificationTracker implements INotificationTracker, Runnable{
 
     private final List<NotificationObserver> observerList;
@@ -16,6 +23,13 @@ public class NotificationTracker implements INotificationTracker, Runnable{
         this.notificationList = new ArrayList<>();
     }
 
+    /**
+     * Add a list of unsorted notifications to notificationList.
+     * This method is always called when starting the program and
+     * importing the existing notifications
+     * @param unsortedNotifications a unsorted notification list
+     * @return true if all notifications are added successfully
+     */
     public boolean addNotifications(List<Notification> unsortedNotifications) {
         for (Notification notification : unsortedNotifications) {
             if (!addNotification(notification)) {
@@ -26,6 +40,11 @@ public class NotificationTracker implements INotificationTracker, Runnable{
     }
 
     // TODO: should this method be private?
+    /**
+     * Insert a notification to notificationList, based on the notification time.
+     * @param newNotification the notification that will be added
+     * @return true if the new notification is added successfully
+     */
     public boolean addNotification(Notification newNotification) {
         LocalDateTime newNotificationTime = newNotification.getNotificationTime();
         for (int i = 0; i < notificationList.size(); i++) {
@@ -39,15 +58,32 @@ public class NotificationTracker implements INotificationTracker, Runnable{
         return true;
     }
 
+    /**
+     * Delete a notification in notificationList with
+     * associatedId being id and notificationTime being time
+     * @param id the associatedId of the target notification
+     * @param time the notification time of the target notification
+     * @return true if the corresponding notification is deleted successfully
+     */
     public boolean deleteNotification(int id, LocalDateTime time) {
         return notificationList.removeIf(notification ->
                 id == notification.getAssociatedId() && time == notification.getNotificationTime());
     }
 
+    /**
+     * Delete all notifications in notificationList with associatedId being id
+     * @param id the associated id that decide which notifications to delete
+     * @return true if all notifications are deleted successfully
+     */
     public boolean deleteNotifications(int id) {
         return notificationList.removeIf(notification -> id == notification.getAssociatedId());
     }
 
+    /**
+     * update the coming notification to all observers in the observerList
+     * @param notification the coming notification that needs to be sent out
+     * @return true if all observers send out the notification successfully
+     */
     @Override
     public boolean updateAll(Notification notification) {
         for (NotificationObserver observer : observerList) {
@@ -58,8 +94,15 @@ public class NotificationTracker implements INotificationTracker, Runnable{
         return true;
     }
 
+    /**
+     * Keep tracking the earliest notification.
+     * If it's time to send the notification,
+     * update all observers in observerList with the notification,
+     * and then remove this notification from notificationList
+     */
     @Override
     public void run() {
+        // TODO: rerun the run() when notificationList is no longer empty
         while (!notificationList.isEmpty()) {
             // remove overdue notifications
             if (notificationList.get(0).getNotificationTime().isAfter(LocalDateTime.now())) {
