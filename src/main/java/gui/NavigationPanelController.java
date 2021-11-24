71907fefba27ebe6cf9b87ab3a0893ea35ad3803
valyippee <valerieyipfh@gmail.com>
2021-11-24 01:54:10 -0500

    added configurations for javafx in build gradle file and basics for gui

---
+++ src/main/java/gui/NavigationPanelController.java
@@ -0,0 +1,49 @@
+package gui;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class NavigationPanelController {
+
+    @FXML
+    private JFXButton calendar;
+
+    @FXML
+    private JFXButton home;
+
+    @FXML
+    private JFXButton pomodoro;
+
+    @FXML
+    private JFXButton settings;
+
+    @FXML
+    private JFXButton todolist;
+
+    @FXML
+    void enterCalendarPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enterHomePage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enterPomodoroPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enterSettingsPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enterTodoListPage(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module sample {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires com.calendarfx.view;
+    requires com.jfoenix;
+
+    opens gui;
+}
