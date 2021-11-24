050db537f7af81a2a5eb38b7ffb818c52cc530d6
Junru Lin <junru.lin@mail.utoronto.ca>
2021-11-24 10:26:03 -0500

    updated NotificationFormatter

--- src/main/java/services/notification_system/NotificationFormatter.java
+++ src/main/java/services/notification_system/NotificationFormatter.java
@@ -20,11 +20,14 @@ public class NotificationFormatter implements NotificationFormat{
 
     @Override
     public String formatTaskNotificationMessage(String taskName, LocalDateTime deadline) {
-        return null;
+        return "You have an upcoming task due at" + deadline.toString();
     }
 
     @Override
     public String formatEventNotificationMessage(String eventName, LocalDate date, LocalTime startTime, LocalTime endTime) {
-        return null;
+        return "You have an upcoming event " + eventName +
+                " from " + startTime.toString() +
+                " to " + endTime.toString() +
+                " on " + date.toString();
     }
 }
