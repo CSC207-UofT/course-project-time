173afb4648dbfcdc3cb92bd97b2ef55cb40b3ced
Junru Lin <junru.lin@mail.utoronto.ca>
2021-11-25 16:38:20 -0500

    implemented some classes and added documentations

--- src/main/java/services/notification_system/NotificationSettingsManager.java
+++ src/main/java/services/notification_system/NotificationSettingsManager.java
@@ -1,25 +1,37 @@
 package services.notification_system;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class NotificationSettingsManager implements NotificationSettings{
     private Map<String, Boolean> notificationSettings;
 
-    public NotificationSettingsManager(Map<String, Boolean> notificationSettings) {
-        this.notificationSettings = notificationSettings;
+    public NotificationSettingsManager() {
+        loadNotificationSettings();
     }
 
     public Map<String, Boolean> getNotificationSettings() {
         return notificationSettings;
     }
 
+    /***
+     * Load notification settings from the database
+     */
     @Override
     public void loadNotificationSettings() {
-
+        notificationSettings = new HashMap<>();
+        notificationSettings.put("EmailEnabled", true);
+        notificationSettings.put("DesktopEnabled", true);
+        // TODO: Load settings from the database
     }
 
+    /***
+     * Update notificationSettings and save the new setting to the data base
+     * @param newSettings the new notification settings
+     */
     @Override
     public void saveNewSettings(Map<String, Boolean> newSettings) {
-
+        notificationSettings = newSettings;
+        // TODO: save to the datebase
     }
 }
