4848f4a992179b2130b128cd066574a7006cddac
Emily Chang <emilychang2002@gmail.com>
2021-12-02 00:35:49 -0500

    windows notifications

--- src/main/java/services/notification_sending/DesktopNotificationPresenter.java
+++ src/main/java/services/notification_sending/DesktopNotificationPresenter.java
@@ -1,14 +1,27 @@
 package services.notification_sending;
 
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+
 /**
  * Sends out desktop push notifications.
  * Keeps a record of whether the user enabled the desktop notification settings.
  */
 public class DesktopNotificationPresenter implements NotificationPresenter, SettingsRegistry {
     private boolean enabled;
+    private SystemTray tray = SystemTray.getSystemTray();
 
     @Override
     public void presentNotification(String message) {
+        try{
+            Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("/resources/icon.png"));
+            TrayIcon trayIcon = new TrayIcon(image, "Time");
+            trayIcon.setImageAutoSize(true);
+            tray.add(trayIcon);
+            trayIcon.displayMessage("Time", message, MessageType.INFO);
+        } catch(AWTException e) {
+            System.err.print(e);
+        }
 
     }
 
