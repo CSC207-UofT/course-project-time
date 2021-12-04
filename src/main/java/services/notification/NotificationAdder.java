3278d0d60a4f4eefc7e03ff7c931537de964781c
Junru Lin <junru.lin@mail.utoronto.ca>
2021-12-04 10:55:55 -0500

    changed the documentations to make it clearer

--- src/main/java/services/notification/NotificationAdder.java
+++ src/main/java/services/notification/NotificationAdder.java
@@ -7,7 +7,8 @@ public class NotificationAdder {
 
     /***
      * Create an event notification using the date from eventData and the event id eventId
-     * @param eventData the event data from the user
+     * @param eventData the event data passed down from the user interface
+     *                  when the user creates a new event
      * @param eventId the id of the event
      */
     public void createNotification(CalendarEventModel eventData, long eventId) {
@@ -15,7 +16,8 @@ public class NotificationAdder {
 
     /***
      * Create a task notification using the date from taskData and the task id tasktId
-     * @param taskData the task data from the user
+     * @param taskData the task data passed down from the user interface
+     *                 when the user creates a new event
      * @param taskId the id of the task
      */
     public void createNotification(TodoListTaskCreationModel taskData, long taskId) {
