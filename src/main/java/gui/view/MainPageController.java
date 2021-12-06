33fb6eba079517219053f99bc2851680b80a09d9
Tahseen <tarinfinite@gmail.com>
2021-12-06 01:05:39 -0500

    gui - main page and settings page

--- src/main/java/gui/view/MainPageController.java
+++ src/main/java/gui/view/MainPageController.java
@@ -2,6 +2,7 @@ package gui.view;
 
 import com.jfoenix.controls.JFXDrawer;
 import gui.utility.NavigationHelper;
+import gui.view_model.ViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -9,7 +10,7 @@ import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainPageController implements Initializable {
+public class MainPageController implements Initializable, ViewModelBindingController {
 
     @FXML
     private JFXDrawer collapsedNavPanel;
@@ -24,5 +25,10 @@ public class MainPageController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         NavigationHelper.initializeNavPanel(extendedNavPanel, collapsedNavPanel);
+    }
+
+    @Override
+    public void init(ViewModel viewModel) {
+
     }
 }
--- src/main/java/gui/view/SettingsController.java
+++ src/main/java/gui/view/SettingsController.java
@@ -2,6 +2,7 @@ package gui.view;
 
 import com.jfoenix.controls.JFXDrawer;
 import gui.utility.NavigationHelper;
+import gui.view_model.ViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -9,7 +10,7 @@ import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SettingsController implements Initializable {
+public class SettingsController implements Initializable, ViewModelBindingController {
 
     @FXML
     private JFXDrawer collapsedNavPanel;
@@ -23,5 +24,10 @@ public class SettingsController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         NavigationHelper.initializeNavPanel(extendedNavPanel, collapsedNavPanel);
+    }
+
+    @Override
+    public void init(ViewModel viewModel) {
+
     }
 }
