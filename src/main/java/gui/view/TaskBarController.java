b55e5c67860de95ba8d9ad3f5deba0d674e8b69c
Jenci <jenci2114@gmail.com>
2021-12-06 17:47:41 -0500

    Finished page outline for task info

--- src/main/java/gui/view/TaskBarController.java
+++ src/main/java/gui/view/TaskBarController.java
@@ -6,9 +6,13 @@ import gui.utility.NavigationHelper;
 import gui.view_model.ViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,15 +29,38 @@ public class TaskBarController implements Initializable, ViewModelBindingControl
     private AnchorPane mainBackground;
 
     @FXML
-    private TextField taskName;
+    private Text taskName;
 
     @FXML
-    private TextArea taskDetail;
+    private DatePicker dueDate;
+
+    @FXML
+    private TextField dueTimeHours;
+
+    @FXML
+    private TextField dueTimeMinutes;
+
+    @FXML
+    private TextField duration;
+
+    @FXML
+    private ToggleButton completed;
+
+    @FXML
+    private Button addSubtask;
+
+    @FXML
+    private Button scheduleAsEvent;
+
+    @FXML
+    private Button updateTask;
+
+    @FXML
+    private Button deleteTask;
 
     @Override
     public void init(ViewModel viewModel) {
-        taskName.setText("Name of Task");
-        taskDetail.setText("Detail of this task will be shown here");
+        taskName.setText("name of task");
     }
 
     @Override
