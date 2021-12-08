97e4daf5772e0295622286524652421734ba5dd7
Junru Lin <junru.lin@mail.utoronto.ca>
2021-12-08 03:41:26 -0500

    implemented NOtificationTracker

--- src/main/java/services/notification/NotificationTracker.java
+++ src/main/java/services/notification/NotificationTracker.java
@@ -2,16 +2,14 @@ package services.notification;
 
 import datagateway.notification.NotificationManager;
 import datagateway.notification.NotificationReader;
-import entity.Notification;
 import services.notificationsending.NotificationPresenter;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class NotificationTracker implements Runnable {
     private final List<NotificationPresenter> observers;
-    private NotificationManager notificationManager;
+    private final NotificationManager notificationManager;
     private NotificationReader upcomingNotification;
 
     public NotificationTracker(List<NotificationPresenter> observers,
@@ -22,52 +20,50 @@ public class NotificationTracker implements Runnable {
     }
 
     /**
-     * Delete a notification in notificationList with
-     * associatedId being id and notificationTime being time
-     * @param id the associatedId of the target notification
-     * @param time the notification time of the target notification
-     */
-    public void deleteNotification(int id, LocalDateTime time) {
-
-    }
-
-    /**
-     * Delete all notifications in notificationList with associatedId being id
-     * @param id the associated id that decide which notifications to delete
-     */
-    public void deleteNotifications(int id) {
-    }
-
-    /***
-     * Insert a notification to notificationList, based on the notification time.
-     * @param newNotification the notification that will be added
-     */
-    public void addNotification(Notification newNotification) {
-
-    }
-
-    /***
-     * Look up the notification with the associated id in the notificationList.
-     * If the notification exists, return the notification time in advance of this notification.
-     * If the notification does not exist, return null.
-     * @param associatedId the associated id of the notification
-     * @return notification time in advance if the notification exists;
-     *         null if the notification does not exists.
-     */
-    public Duration getNotificationTimeInAdvance(long associatedId) {
-
-        return null;
-    }
-
-    /**
-     * Keep tracking the earliest notification.
+     * Keep tracking the upcoming notification.
      * If it's time to send the notification,
      * update all observers in observerList with the notification,
-     * and then remove this notification from notificationList
+     * and then update the upcomingNotification
      */
     @Override
     public void run() {
-        // TODO: rerun the run() when notificationList is no longer empty
+        while (true) {
+            if (upcomingNotification != null) {
+                // make a weak condition to send notification
+                if (upcomingNotification.getNotificationDateTime().isAfter(LocalDateTime.now())) {
+                    updateObservers(upcomingNotification.getMessage());
+                    updateUpcomingNotification();
+                }
+            }
+        }
+
+    }
+
+    /***
+     * Helper method used by <run> method to update the <upcomingNotification>
+     *     after a notification is sent out.
+     *     If there is no upcoming notification, set <upcomingNotification>
+     *         to be null
+     */
+    private void updateUpcomingNotification() {
+        List<NotificationReader> notificationReaders = notificationManager.getAllNotifications();
+        if (notificationReaders.isEmpty()) {
+            this.upcomingNotification = null;
+        } else {
+            NotificationReader earliestSoFar = notificationReaders.get(0);
+
+            for (NotificationReader nr : notificationReaders) {
+                if (nr.getNotificationDateTime().isBefore(earliestSoFar.getNotificationDateTime())){
+                    earliestSoFar = nr;
+                }
+            }
+
+            if (earliestSoFar.getNotificationDateTime().isBefore(LocalDateTime.now())) {
+                this.upcomingNotification = earliestSoFar;
+            } else {
+                this.upcomingNotification = null;
+            }
+        }
 
     }
 
