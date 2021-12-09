62408bd5fcbe0af32df0b0c7ed7ce3df154b55d5
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-12-08 23:06:31 -0500

    improved strategy construction message

--- src/main/java/consoleapp/EventQuery.java
+++ src/main/java/consoleapp/EventQuery.java
@@ -128,7 +128,7 @@ public class EventQuery {
         Scanner sc = new Scanner(System.in);
         String input;
         do {
-            System.out.print("Input what day of the week you want the event to reoccur: ");
+            System.out.print("Input what day of the week you want the event to reoccur (full name like 'Monday'): ");
             input = sc.nextLine().toLowerCase();
         } while (!daysSet.contains(input));
 
